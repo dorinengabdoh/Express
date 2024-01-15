8313aa68a6a80896db5fa3d8ce68241cb63dd073
dorinengabdoh <ngabdohdorine@gmail.com>
2024-01-15 10:31:10 +0100

    make some change

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const express = require('express')
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 const app = express()
+app.use('/', indexRouter)
+app.use('/users', userRouter)
 module.exports =app
