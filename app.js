ff443cb36339d744228e02280fb28b07019800f2
dorinengabdoh <ngabdohdorine@gmail.com>
2024-01-15 12:40:17 +0100

    strt evrything back

--- app.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const express = require('express')
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
-const app = express()
-app.use('/', indexRouter)
-app.use('/users', userRouter)
-module.exports =app
