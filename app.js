2846389aef34fd55633c65b2f1a5be1631f34215
dorinengabdoh <ngabdohdorine@gmail.com>
2024-01-15 09:02:46 +0100

    fix errors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict'
 const express = require('express')
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
 const app = express()
 module.exports =app
--- routes/hello.js
+++ routes/hello.js
@@ -1,5 +1,7 @@
 var express  = require('express')
 var router = express.Router();
+var indexRouter = require('./routes/index')
+var helloRRouter = require('./routes/hello');
 router.get('/', function(req, res, next) {
   const greeting = 'greeting' in req.query ?
   req.query.greeting : 
